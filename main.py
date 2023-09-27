ed34120cc7fa2e8e790534f14c8fd02ad8cbaf85
VladiSlave-Fesik <vladfesik36@gmail.com>
2023-09-27 18:55:03 +0300

    1.0

--- main.py
+++ main.py
@@ -1,85 +1,124 @@
-import requests
 import os
+from shutil import copy
+import customtkinter as ctk
+import threading
+import parser
+from PIL import Image
 
 
-def check_url_exists(url):
-    try:
-        response = requests.head(url)
-        return response.status_code == 200
-    except requests.exceptions.RequestException:
-        return False
+class SteamAssetsGUI(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('800x600')
+        self.title('SteamAssets')
+        self.wm_iconbitmap('steam.ico')
+        self.protocol('WM_DELETE_WINDOW', self.on_close)
+        self.bind_all('<Button-1>', lambda event: event.widget.focus_set())
+        self.bind('<Left>', self.show_previous_image)
+        self.bind('<Right>', self.show_next_image)
 
+        id_label = ctk.CTkLabel(self, text='Enter ID of Steam App', text_color='white')
+        id_label.pack(anchor='w')
 
-def find_available_alt_assets(steam_id):
-    alt_assets_text = '_alt_assets_'
-    existing = []
+        # Create a validation function to allow only digits
+        validate_digits = self.register(self.validate_digits)
 
-    urls = [
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/broadcast_left_panel.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/broadcast_right_panel.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_231x87.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_616x353.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/header.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/hero_capsule.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_600x900.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_hero.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/logo.png',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/page_bg_raw.jpg',
-        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_467x181.jpg',
-    ]
+        self.id_entry = ctk.CTkEntry(self, validate="key", validatecommand=(validate_digits, '%P'))
+        self.id_entry.pack(anchor='w')
 
-    urls.sort()
+        search_button = ctk.CTkButton(self, text='Search', command=self.start_search_func)
+        search_button.pack(anchor='w', pady=20)
 
-    for url_template in urls:
-        url = url_template.format(steam_id)
-        if check_url_exists(url):
-            existing.append(url)
-            alt_assets = find_available_alt_assets_for_url(url, alt_assets_text)
-            existing.extend(alt_assets)
+        self.progress_bar = ctk.CTkProgressBar(self, mode='indeterminate')
 
-    return existing
+        self.image_index = 0
+        self.images = []
+        self.images_names = []
+        self.image_labels = []
 
-
-def find_available_alt_assets_for_url(url, alt_assets_text):
-    alt_assets = []
-    photo_name = url.split('/')[-1].split('.')[0]
-    i = 0
-
-    while True:
-        alt_url = url.replace(photo_name, f"{photo_name}{alt_assets_text}{i}")
-        if check_url_exists(alt_url):
-            alt_assets.append(alt_url)
+    def validate_digits(self, new_value):
+        # Validate function to allow only digits
+        if new_value.isdigit() or new_value == '':
+            return True
         else:
-            if i >= 2:
-                break
-        i += 1
+            return False
 
-    return alt_assets
+    def start_search_func(self):
+        self.show_progress_bar()
+        self.progress_bar.start()
 
+        # Start test_func in a separate thread
+        thread = threading.Thread(target=self.search_func)
+        thread.start()
 
-def download_image(url, save_dir):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            file_name = os.path.basename(url)
-            save_path = os.path.join(save_dir, file_name)
+    def search_func(self):
+        self.images = []  # Clear existing images
+        for url in parser.find_available_alt_assets(self.id_entry.get()):
+            image = parser.download_image(url, 'temp')
+            if image:
+                self.images.append(image)
+                self.images_names.append(url.split('/')[-1].split('.')[0])
 
-            # Save the file
-            with open(save_path, 'wb') as file:
-                file.write(response.content)
-            print(f'Image successfully downloaded and saved to {save_path}')
-            return save_path
-        else:
-            print(f'Error downloading image. Status code: {response.status_code}')
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f'Error during request: {e}')
-        return None
+        self.show_images()
+        self.progress_bar.stop()
+        self.hide_progress_bar()
 
+    def hide_progress_bar(self):
+        self.progress_bar.pack_forget()
 
-steam_id = 304930
-existing_urls = find_available_alt_assets(steam_id)
+    def show_progress_bar(self):
+        self.progress_bar.pack(padx=10, pady=10)
 
-print(f'URLs len = {len(existing_urls)}')
-for url in existing_urls:
-    download_image(url, 'temp')
+    def show_images(self):
+        # Clear existing image labels
+        for label in self.image_labels:
+            label.pack_forget()
+
+        # Display the current image, title, and navigation buttons
+        if self.images:
+            current_image = Image.open(self.images[self.image_index])
+            current_image = ctk.CTkImage(current_image, size=current_image.size)
+
+            image_label = ctk.CTkLabel(self, image=current_image, text='')
+            image_label.image = current_image
+            image_label.pack(anchor='center')
+
+            title_label = ctk.CTkLabel(self, text=self.images_names[self.image_index])
+            title_label.pack(anchor='center')
+
+            save_button = ctk.CTkButton(self, text='Save', command=self.save_image)
+            save_button.pack(anchor='center')
+
+            prev_button = ctk.CTkButton(self, text='Previous', command=self.show_previous_image)
+            prev_button.pack(side='left')
+
+            next_button = ctk.CTkButton(self, text='Next', command=self.show_next_image)
+            next_button.pack(side='right')
+
+            # Store the labels to remove them later
+            self.image_labels = [image_label, title_label, save_button, prev_button, next_button]
+
+    def show_next_image(self, *args):
+        if self.images and self.image_index < len(self.images) - 1:
+            self.image_index += 1
+            self.show_images()
+
+    def show_previous_image(self, *args):
+        if self.images and self.image_index > 0:
+            self.image_index -= 1
+            self.show_images()
+
+    def save_image(self):
+        if self.images:
+            file_name = self.images[self.image_index]
+            file_name = os.path.basename(file_name)
+            copy(os.path.join('temp', file_name), os.path.join('downloads', file_name))
+
+    def on_close(self):
+        for file in os.listdir('temp'):
+            os.remove(os.path.join('temp', file))
+        self.destroy()
+
+if __name__ == '__main__':
+    app = SteamAssetsGUI()
+    app.mainloop()
---
+++ parser.py
@@ -0,0 +1,78 @@
+import requests
+import os
+
+
+def check_url_exists(url):
+    try:
+        response = requests.head(url)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
+
+def find_available_alt_assets(steam_id):
+    alt_assets_text = '_alt_assets_'
+    existing = []
+
+    urls = [
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/broadcast_left_panel.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/broadcast_right_panel.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_231x87.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_616x353.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/header.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/hero_capsule.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_600x900.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_hero.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/logo.png',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/page_bg_raw.jpg',
+        'https://cdn.cloudflare.steamstatic.com/steam/apps/{}/capsule_467x181.jpg',
+    ]
+
+    urls.sort()
+
+    for url_template in urls:
+        url = url_template.format(steam_id)
+        if check_url_exists(url):
+            existing.append(url)
+            alt_assets = find_available_alt_assets_for_url(url, alt_assets_text)
+            existing.extend(alt_assets)
+
+    return existing
+
+
+def find_available_alt_assets_for_url(url, alt_assets_text):
+    alt_assets = []
+    photo_name = url.split('/')[-1].split('.')[0]
+    i = 0
+
+    while True:
+        alt_url = url.replace(photo_name, f"{photo_name}{alt_assets_text}{i}")
+        if check_url_exists(alt_url):
+            alt_assets.append(alt_url)
+        else:
+            if i >= 2:
+                break
+        i += 1
+
+    return alt_assets
+
+
+def download_image(url, save_dir):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            file_name = os.path.basename(url)
+            save_path = os.path.join(save_dir, file_name)
+
+            # Save the file
+            with open(save_path, 'wb') as file:
+                file.write(response.content)
+            print(f'Image successfully downloaded and saved to {save_path}')
+            return save_path
+        else:
+            print(f'Error downloading image. Status code: {response.status_code}')
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f'Error during request: {e}')
+        return None
+
